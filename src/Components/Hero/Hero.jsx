c8f9e30e1fee3f4f15dfb114d0e264cb5f01be9d
102sunnysharma <102sunnysharma@gmail.com>
2026-03-26 09:57:11 +0530

    adding some new features

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Hero.css";
+import hand_icon from "../../assets/hand_icon.png";
+import arrow_icon from "../../assets/arrow_icon.png"
+import men from "../../assets/men.webp"
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      {/* left section */}
+      <div className="hero-left">
+        <h2>Best Deals!Best Prices!</h2>
+
+        <div className="hero-hand-icon">
+          <img src={hand_icon} alt="" height="60px" />
+        </div>
+        
+        <p>Collections</p>
+        <p>for everyone</p>
+        <div className="hero-latest-btn">
+          <div>Latest Collections</div>
+          <img src={arrow_icon} alt="" height="30px" />
+        </div>
+      </div>
+
+      {/* right section */}
+      <div className="hero-right">
+        <img src={men} alt=""  height="500px"/>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Hero;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.webp";
 import cart_icon from "../../assets/cart_icon.png"
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 
@@ -15,14 +16,14 @@ export const Navbar = () => {
         <p>Shopify</p>
       </div>
       <ul className="nav-menu">
-        <li onClick={() => {setMenu("home")}}>Home {menu === "home" ? <hr/> : <></> }</li>
-        <li onClick={() => {setMenu("mens")}}>Men {menu === "mens" ? <hr/> : <></> }</li>
-        <li onClick={() => {setMenu("womens")}}>Women {menu === "womens" ? <hr/> : <></> }</li>
-        <li onClick={() => {setMenu("kids")}}>Kids{menu === "kids" ? <hr/> : <></> }</li>
+        <li onClick={() => {setMenu("home")}}><Link style={{textDecoration:"none",color:"#626262"}} to = "/">Home</Link> {menu === "home" ? <hr/> : <></> }</li>
+        <li onClick={() => {setMenu("mens")}}><Link style={{textDecoration:"none",color:"#626262"}} to="/mens">Men</Link> {menu === "mens" ? <hr/> : <></> }</li>
+        <li onClick={() => {setMenu("womens")}}><Link style={{textDecoration:"none",color:"#626262"}} to="/womens">Women</Link>{menu === "womens" ? <hr/> : <></> }</li>
+        <li onClick={() => {setMenu("kids")}}><Link style={{textDecoration:"none",color:"#626262"}} to="/kids">Kids</Link>{menu === "kids" ? <hr/> : <></> }</li>
       </ul>
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="" height={"40px"} />
+        <Link to="/login"> <button>Login</button></Link>
+        <Link to="/cart"><img src={cart_icon} alt="" height={"40px"} /></Link>
         <div className="nav-cart-count">0</div>
       </div>
     </div>
