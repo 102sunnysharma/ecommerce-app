fcc319ebb759b23451f296c8447166f99abb54b1
102sunnysharma <102sunnysharma@gmail.com>
2026-03-29 17:47:50 +0530

    adding features

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { ShopContext } from '../Context/ShopContext';
+import {useParams} from "react-router-dom"
+import BredCrums from '../Components/Bredcrums/BredCrums';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find((e) => e.id === Number(productId))
+
+  if (!product) return null;
+
   return (
-    <div>Product</div>
+    <div>
+      <BredCrums product = {product} />
+      <ProductDisplay product = {product}/>
+    </div>
   )
 }
 
